306b08c5cacf9f758556d58b2338896823310a04
Andrea-Lulli <andrea.lulli89@gmail.com>
2023-01-16 17:16:58 +0100

    create card

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,9 @@
     <div class="home">
         <h1>Lista Film</h1>
     </div>
-    <div class="box-card">
+    <div class="box-card w-75 row ">
         @foreach ($all_movies as $elem)
-            <div class="card">
+            <div class="card col-3  m-3 p-2">
                 <h4>{{ $elem->title }}</h4>
                 <p>{{ $elem->nationality }}</p>
                 <p>{{ $elem->date }}</p>
@@ -15,7 +15,7 @@
         @endforeach
     </div>
 
-    <div>
+    <div class="">
         <h2>Titolo che iniziano per T</h2>
         @foreach ($Movie_filtered as $elem)
             <p>{{ $elem->title }}</p>
